8cd3823a2586f46fcc6e9fdca9a3db333e3acb2f
Insearchman <insearchman@gmail.com>
2026-07-06 04:18:09 +0300

    [SceneSwitch] preparing

---
+++ Assets/_Project/Develop/Runtime/Infrastructure/EntryPoint/ProjectContextRegistrations.cs
@@ -0,0 +1,58 @@
+﻿using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilities.AssetsManagment;
+using Assets._Project.Develop.Runtime.Utilities.CoroutinesManagment;
+using Assets._Project.Develop.Runtime.Utilities.LoadingScreen;
+using Assets._Project.Develop.Runtime.Utilities.SceneManagment;
+using Object = UnityEngine.Object;
+
+namespace Assets._Project.Develop.Runtime.Infrastructure.EntryPoint
+{
+    public class ProjectContextRegistrations
+    {
+        public static void Process(DIContainer container)
+        {
+            container.RegisterAsSingle<ICoroutinesPerformer>(CreateCoroutinesPerformer);
+
+            container.RegisterAsSingle(CreateResourcesAssetsLoader);
+
+            container.RegisterAsSingle(CreateSceneLoaderService);
+
+            container.RegisterAsSingle(CreateSceneSwitcherService);
+
+            container.RegisterAsSingle<ILoadingScreen>(CreateLoadingScreen);
+        }
+
+        private static SceneSwitcherService CreateSceneSwitcherService(DIContainer c)
+            => new SceneSwitcherService(
+                c.Resolve<SceneLoaderService>(),
+                c.Resolve<ILoadingScreen>(),
+                c);
+
+        private static SceneLoaderService CreateSceneLoaderService(DIContainer c)
+            => new SceneLoaderService();
+
+
+        private static ResourcesAssetsLoader CreateResourcesAssetsLoader(DIContainer c)
+            => new ResourcesAssetsLoader();
+
+        private static CoroutinesPerformer CreateCoroutinesPerformer(DIContainer c)
+        {
+            ResourcesAssetsLoader resourcesAssetsLoader = c.Resolve<ResourcesAssetsLoader>();
+
+            CoroutinesPerformer coroutinesPerformerPrefab = resourcesAssetsLoader
+                .Load<CoroutinesPerformer>("Utilities/CoroutinesPerformer");
+
+            return Object.Instantiate(coroutinesPerformerPrefab);
+        }
+
+        private static StandardLoadingScreen CreateLoadingScreen(DIContainer c)
+        {
+            ResourcesAssetsLoader resourcesAssetsLoader = c.Resolve<ResourcesAssetsLoader>();
+
+            StandardLoadingScreen standardLoadingScreenPrefab = resourcesAssetsLoader
+                .Load<StandardLoadingScreen>("Utilities/StandardLoadingScreen");
+
+            return Object.Instantiate(standardLoadingScreenPrefab);
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Utilities/SceneManagment/Scenes.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets._Project.Develop.Runtime.Utilities.SceneManagment
+{
+    public class Scenes
+    {
+        public const string GameEntryPoint = nameof(GameEntryPoint);
+        public const string MainMenu = nameof(MainMenu);
+        public const string Entrance = nameof(Entrance);
+        public const string Elevator = nameof(Elevator);
+        public const string Shop = nameof(Shop);
+    }
+}
