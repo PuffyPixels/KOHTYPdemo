5ddbe147bdc92b7f3035da9162180e3e4268dbe6
Insearchman <insearchman@gmail.com>
2026-07-31 19:41:24 +0300

    [MainMenu] Add blinks

--- Assets/_Project/Develop/Runtime/UI/MainMenu/MainMenuScreenPresenter.cs
+++ Assets/_Project/Develop/Runtime/UI/MainMenu/MainMenuScreenPresenter.cs
@@ -1,6 +1,7 @@
 ﻿using Assets._Project.Develop.Runtime.UI.Core;
 using Assets._Project.Develop.Runtime.Utilities.CoroutinesManagment;
 using Assets._Project.Develop.Runtime.Utilities.SceneManagment;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +9,19 @@ namespace Assets._Project.Develop.Runtime.UI.MainMenu
 {
     public class MainMenuScreenPresenter : IPresenter
     {
+        private const int BACKGROUND_SWITCH = 2;
+
         private readonly MainMenuScreenView _screen;
 
         private SceneSwitcherService _sceneSwitcherService;
         private ICoroutinesPerformer _coroutinesPerformer;
 
         private readonly List<IPresenter> _childPresenters = new();
+
+        private Sprite _normal;
+        private Sprite _faded;
+
+        private WaitForSeconds _backgroundWait = new(0.08f);
 
         public MainMenuScreenPresenter(
             MainMenuScreenView screen,
@@ -31,6 +39,10 @@ namespace Assets._Project.Develop.Runtime.UI.MainMenu
             _screen.StartNewGameButtonView.Click += OnStartNewGameButtonClicked;
             _screen.OptionsButtonView.Click += OnOptionsButtonClicked;
             _screen.CloseGameButtonView.Click += OnCloseGameButtonClicked;
+
+            _normal = _screen.Normal;
+            _faded = _screen.Faded;
+            _screen.Background.sprite = _normal;
 
             foreach (IPresenter presenter in _childPresenters)
                 presenter.Initialize();
@@ -67,6 +79,7 @@ namespace Assets._Project.Develop.Runtime.UI.MainMenu
         private void OnStartNewGameButtonClicked()
         {
             ButtonClickHandler();
+            _coroutinesPerformer.StartPerform(BackgroundSwitch());
             _coroutinesPerformer.StartPerform(_sceneSwitcherService.ProcessSwitchTo(Scenes.Entrance));
         }
 
@@ -87,6 +100,18 @@ namespace Assets._Project.Develop.Runtime.UI.MainMenu
             _screen.StartNewGameButtonView.SetActive(false);
             _screen.OptionsButtonView.SetActive(false);
             _screen.CloseGameButtonView.SetActive(false);
+        }
+
+        private IEnumerator BackgroundSwitch()
+        {
+            for (int i = 0; i < BACKGROUND_SWITCH; i++)
+            {
+                _screen.Background.sprite = _faded;
+                yield return _backgroundWait;
+
+                _screen.Background.sprite = _normal;
+                yield return _backgroundWait;
+            }
         }
     }
 }
--- Assets/_Project/Develop/Runtime/UI/MainMenu/MainMenuScreenView.cs
+++ Assets/_Project/Develop/Runtime/UI/MainMenu/MainMenuScreenView.cs
@@ -1,5 +1,6 @@
 ﻿using Assets._Project.Develop.Runtime.UI.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets._Project.Develop.Runtime.UI.MainMenu
 {
@@ -9,5 +10,8 @@ namespace Assets._Project.Develop.Runtime.UI.MainMenu
         [field: SerializeField] public MainMenuButtonView StartNewGameButtonView { get; private set; }
         [field: SerializeField] public MainMenuButtonView OptionsButtonView { get; private set; }
         [field: SerializeField] public MainMenuButtonView CloseGameButtonView { get; private set; }
+        [field: SerializeField] public Image Background { get; private set; }
+        [field: SerializeField] public Sprite Normal { get; private set; }
+        [field: SerializeField] public Sprite Faded { get; private set; }
     }
 }
